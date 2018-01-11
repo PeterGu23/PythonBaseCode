208d96b2b66cf04ed1c31943b2ed828c047c4a12
PeterGu23 <gucaca@yeah.net>
2018-01-11 13:07:56 +0800

    add all variables

---
+++ PythonVariables/if_adult.py
@@ -0,0 +1,6 @@
+age = input("请输入你的年龄：")
+
+if int(age) >= 18:
+    print ("你是成年人。")
+else:
+    print ("你是未成年")
---
+++ PythonVariables/typeChange.py
@@ -0,0 +1,16 @@
+a = 100
+b = '200'
+
+print("str(a) + b = %s"%(str(a)+b))
+print ("a+int(b) = %d" %(a+int(b)))
+
+
+c = "100+200"
+print (c)
+
+print (eval(c))
+
+
+print ("100的16进制是：%s" %  hex(a))
+print ("100的8进制是：%s" %  oct(a))
+
